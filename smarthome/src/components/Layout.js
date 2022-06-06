2598e93eba056391bb44d34108bdf10006710343
SiemLuc <48807736+SiemLuc@users.noreply.github.com>
2022-06-07 00:51:53 +0200

    Fixed Sidebar overlap

--- smarthome/src/components/Layout.js
+++ smarthome/src/components/Layout.js
@@ -5,12 +5,12 @@ const Layout = ({children}) => (
     <div className='flex'>
         <TopBar />
         <div className='flex flex-row'>
-            <SideBar />
             <div className='content'>
                 <div className='m-2 flex flex-col'>
                     {children}
                 </div>
             </div>
+            <SideBar />
         </div>
     </div>
 )
