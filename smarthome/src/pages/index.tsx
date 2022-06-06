5c25a6b17e9328f2caa919e322e02f023e29d416
SiemLuc <48807736+SiemLuc@users.noreply.github.com>
2022-06-07 00:54:06 +0200

    Update index.tsx

--- smarthome/src/pages/index.tsx
+++ smarthome/src/pages/index.tsx
@@ -1,24 +1,6 @@
-import { prisma } from "../../lib/prisma"
-import dynamic from "next/dynamic"
-import { DetectionProps } from "../components/Detection"
-
-const CatPage = dynamic(() => import("../pages/CatPage"), { ssr:false})
-
-export async function getServerSideProps() {
-  const detections = await prisma.detection.findMany({
-    select: {
-      id: true,
-      objects: true,
-      detectedAt: true,
-      image: false
-    }
-  })
-  return { props: { detections } } // will be passed to the page component as props
-}
-
-const Home: React.FC<{detections: DetectionProps[]}> = ({detections}) => {
+const Home = () => {
   return <>
-    <CatPage detections={detections}/>
+  
   </>
 }
 
