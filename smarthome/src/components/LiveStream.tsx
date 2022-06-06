51727c339ef7cd3ca42021cd1c55df1d70abf18d
SiemLuc <48807736+SiemLuc@users.noreply.github.com>
2022-06-07 00:52:09 +0200

    Added LiveStream Component

---
+++ smarthome/src/components/LiveStream.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react'
+import Image from 'next/image'
+import { DetectionProps } from './Detection'
+
+const LiveStream: FC = () => {
+  return (<div className='flex flex-col'>
+    <div className='catcard'>
+        <h1 className='font-bold text-center mb-3'>Window</h1>
+        <div className="flex flex-row justify-center items-center">
+          <Image className='rounded-xl' src="/../public/img/window.jpeg" height={'350'} width={'270'} alt="Sammy" />
+        </div>
+      </div>
+
+      <div className='catcard'>
+        <h1 className='font-bold text-center mb-3'>Food tray</h1>
+        <div className="flex flex-row justify-center items-center">
+          <Image className='rounded-xl' src="/../public/img/tray.jpeg" height={'350'} width={'270'} alt="Sammy" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LiveStream
