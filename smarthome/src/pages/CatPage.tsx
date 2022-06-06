c8f8568127d330993bcee08361239a48bf5e5503
SiemLuc <48807736+SiemLuc@users.noreply.github.com>
2022-06-07 00:53:15 +0200

    Added Serversiderendering

--- smarthome/src/pages/CatPage.tsx
+++ smarthome/src/pages/CatPage.tsx
@@ -1,10 +1,25 @@
 import React, { FC } from 'react'
-import Detection, { DetectionProps } from '../components/Detection'
+import { prisma } from '../../lib/prisma'
+import { DetectionProps } from '../components/Detection'
+import DetectionLog from '../components/DetectionLog'
+import DetectionPreview from '../components/DetectionPreview'
+
+export async function getServerSideProps() {
+	const detections = await prisma.detection.findMany({
+	  select: {
+		id: true,
+		objects: true,
+		detectedAt: true,
+		image: false
+	  }
+	})
+	return { props: { detections } } // will be passed to the page component as props
+  }
 
 const CatPage: FC<{ detections: DetectionProps[] }> = ({ detections }) => (
- 	<>
-	 	{detections?.map(d => <Detection key={d.id} {...d} />)}
-    </>
+	<div className="flex flex-row">
+		<DetectionLog detections={detections}/>
+	</div>
 )
 
 export default CatPage
