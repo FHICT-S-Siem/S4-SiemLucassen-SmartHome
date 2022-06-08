8dd7a0e28a79018a3dd998fc78e758f812d2ec08
SiemLuc <48807736+SiemLuc@users.noreply.github.com>
2022-06-08 03:41:45 +0200

    Updated Epochs to date & relocated time ago code

--- smarthome/src/components/DetectionLog.tsx
+++ smarthome/src/components/DetectionLog.tsx
@@ -1,41 +1,20 @@
 import React, { FC, useContext } from 'react'
-
+import Moment from 'react-moment';
 import { DetectionProps } from './Detection'
 import { Context } from '../pages/Store'
 
 const DetectionLog: FC<{ detections: DetectionProps[]}> = ({detections}) => {
     //set id for the DetectionPreview
     const store = useContext(Context)
- 
+    const showDate = (detectedAt:number) =>{
+      let d = new Date(detectedAt)
+      
+    }
     const handleDetectionPreview = async (id: number) => {
       const res = await fetch(`/api/detection/${id}`)
       const data = await res.json()
       store.dispatch({ type: "setDetections", payload: data})
     }        
-
-    const secondsToElapsedTime = (detectedAt:number) => {
-      const seconds = Math.round((Date.now()-detectedAt*1000)/1000)
-      if (seconds < 60) 
-        return `${seconds} seconds ago`
-      const leftoverSeconds = seconds % 60
-      const minutes = Math.floor(seconds / 60)
-      if (minutes < 60) 
-        return `${minutes} minutes and ${leftoverSeconds} seconds ago`
-      const leftoverMinutes = minutes % 60
-      const hours = Math.floor(minutes / 60)
-      if (hours < 24) 
-        return `${hours} hours and ${leftoverMinutes} minutes ago`
-      const leftoverHours = hours % 24
-      const days = Math.floor(hours / 24)
-      if (days < 7) 
-        return `${days} days and ${leftoverHours} hours ago`
-      const leftoverDays = days % 7
-      const weeks = Math.floor(days / 7)
-      if (days < 365) 
-        return `${weeks} weeks and ${leftoverDays} days ago`
-      const years = Math.round(days / 365)
-      return years + " years ago"
-    }
   return (<>
     <div className='catcard'>
       <div className='overflow-auto w-auto h-auto'>
@@ -44,7 +23,7 @@ const DetectionLog: FC<{ detections: DetectionProps[]}> = ({detections}) => {
             {detections.map(d => 
             <li className='list-item mb-5 rounded-md cursor-pointer bg-secondary w-100  text-white' key={d.id}>
                 <button onClick={() => handleDetectionPreview(d.id)} className='w-full hover:shadow-md'>
-                    {secondsToElapsedTime(d.detectedAt)}
+                    <Moment format='YYYY/MM/DD' unix>{d.detectedAt}</Moment> <br></br>
                 </button>
             </li>
             )} 
--- smarthome/src/components/DetectionPreview.tsx
+++ smarthome/src/components/DetectionPreview.tsx
@@ -4,6 +4,29 @@ import { Context } from '../pages/Store'
 
 const DetectionPreview = () => {
   const store = useContext(Context)
+  const secondsToElapsedTime = (detectedAt:number) => {
+    const seconds = Math.round((Date.now()-detectedAt*1000)/1000)
+    if (seconds < 60) 
+      return `${seconds} seconds ago`
+    const leftoverSeconds = seconds % 60
+    const minutes = Math.floor(seconds / 60)
+    if (minutes < 60) 
+      return `${minutes} minutes and ${leftoverSeconds} seconds ago`
+    const leftoverMinutes = minutes % 60
+    const hours = Math.floor(minutes / 60)
+    if (hours < 24) 
+      return `${hours} hours and ${leftoverMinutes} minutes ago`
+    const leftoverHours = hours % 24
+    const days = Math.floor(hours / 24)
+    if (days < 7) 
+      return `${days} days and ${leftoverHours} hours ago`
+    const leftoverDays = days % 7
+    const weeks = Math.floor(days / 7)
+    if (days < 365) 
+      return `${weeks} weeks and ${leftoverDays} days ago`
+    const years = Math.round(days / 365)
+    return years + " years ago"
+  }
   {console.log(store.state)}
   return (
     
@@ -11,7 +34,7 @@ const DetectionPreview = () => {
       {store.state.detections?.map(d => 
       <div key={d.id}>
         id: {d.id} <br></br>
-        Detected at:  {d.detectedAt}
+        Detected at:  {secondsToElapsedTime(d.detectedAt)}
         
           {d.objects.map(o => 
             <p key={o.id}>
